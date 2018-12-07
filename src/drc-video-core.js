49e18d6b9bb47c778858d1784f9247adef9549da
PAWA <PAWA@dr.dk>
2018-12-07 09:38:44 +0100

    recreate ie11 error with two players.

--- src/drc-video-core.js
+++ src/drc-video-core.js
@@ -3,18 +3,27 @@ var playerFactory = require("@dr/drc-video-core").PlayerFactory;
 /* OnDemand Video Player */
 var videoElement = document.getElementById('video-player');
 
+var videoElement2 = document.getElementById('video-player-2');
+
 var options = {
     videoElement: videoElement,
     slug: "kyst-til-kyst-iii-limfjorden-oest-6-8",
     // channel: dr1
 };
+var options2 = {
+    videoElement: videoElement2,
+    slug: "gintberg-pa-kanten-beredskabsstyrelsen-6-7",
+    // channel: dr1
+};
 
 playerFactory.getPlayer(options, function(player) {
     console.log("use the player object");
-    player.on("play", function(evt){
-        console.log("play was called");
-    });
-    player.play();
 });
+
+
+playerFactory.getPlayer(options2, function(player) {
+    console.log("use the player object");
+});
+
 
 
